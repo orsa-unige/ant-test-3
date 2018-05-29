fab4ccbb17e8efecbda4ede1354b1df0ddf0995d
Davide Ricci <davide.ricci@oapd.inaf.it>
2018-05-29 11:15:47 +0200

    test asker.js to be loaded into node console. does not work

---
+++ asker.js
@@ -0,0 +1,72 @@
+#!/usr/bin/env node
+
+var config = require('./config.json')  /// Configuration file with useful parameters
+
+var wsclient = require('websocket').client; /// Create a websocket client
+ 
+var client = new wsclient();
+
+/// Let's create an object where we will put the random number.
+var mess={}
+
+/// This javascript manages the actor "dummy 1".
+var whoami="console";
+
+/// So let's find all its properties in the configuration file,
+/// and append it in the raw form data.
+mess.actor=config.actor.prop.find(function(o){return o.name==whoami});  
+mess.asker={}
+
+client.on('connectFailed', function(error) {
+    console.log('Connect Error: ' + error.toString());
+});
+
+this.send = function(json){
+    
+    client.on('connect', function(connection) {
+
+        //     console.log('WebSocket Client Connected');
+    //     connection.on('error', function(error) {
+    //         console.log("Connection Error: " + error.toString());
+    //     });
+    //     connection.on('close', function() {
+    //         console.log('echo-protocol Connection Closed');
+    //     });
+    //     connection.on('message', function(message) {
+    //         if (message.type === 'utf8') {
+    //             console.log("Received: '" + message.utf8Data + "'");
+    //         }
+    //     });
+        
+    // function sendNumbers() {
+//        if (connection.connected) {
+
+    //         /// I create a pair or random data.
+    //         var xx = Math.round(Math.random() * 0xFFFFFF);
+    //         var yy = Math.round(Math.random() * 0xFFFFFF);	  
+	    	    
+    //         /// I append the random number to the json    
+    //         //mess.randomnumbers.push({x:xx, y:yy}) /// This adds a pair every loop
+    //     	mess.randomnumbers =[xx, yy]
+
+    mess.asker = json
+
+        console.log("sending: "+JSON.stringify(json))            
+        connection.sendUTF(JSON.stringify(json));
+
+    //         setTimeout(sendNumbers, 3000);
+    //     }
+//        }
+
+    // sendNumbers();
+
+    });
+
+}
+    
+/// Let's take some properties of the websocket server:I need the ip and the port
+var wss = config.actor.prop.find(function(o){return o.name=="websocket_server"});
+ 
+client.connect('ws://'+wss.ip+":"+wss.port, 'echo-protocol');
+
+module.exports = {send: this.send}
